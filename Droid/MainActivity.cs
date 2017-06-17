56420b213e9fd8d602798305d96f38e8e262f69f
boris <borisblanc@gmail.com>
2017-06-17 03:24:47 -0400

    big

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,113 +1,139 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Android.Views;
-using Android.Hardware;
-using Android.Hardware.Camera2;
-using Android.Graphics;
+using Android.Gms.Vision;
+using Android.Support.V4.App;
+
+
+using Android.Util;
+using Android;
+
+using Android.Content;
+using Android.Gms.Vision.Faces;
 using Java.Lang;
-using System.Collections.Generic;
-using Android.Media;
+using System;
+using Android.Runtime;
+using static Android.Gms.Vision.MultiProcessor;
+using Android.Content.PM;
+using Android.Gms.Common;
+using System.Threading.Tasks;
+
 
 
 namespace GrowPea.Droid
 {
     [Activity(Label = "GrowPea", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity, TextureView.ISurfaceTextureListener
+    public class MainActivity : Activity, IFactory
     {
-        CameraDevice _camera;
-		TextureView _textureView;
 
-		//{@link CaptureRequest.Builder} for the camera preview
-		CaptureRequest.Builder mPreviewRequestBuilder;
+        private static readonly string TAG = "FaceTracker";
 
-        // An {@link ImageReader} that handles still image capture.
-        ImageReader mImageReader;
+        private CameraSource mCameraSource = null;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        private CameraSourcePreview mPreview;
+        private GraphicOverlay mGraphicOverlay;
+
+        public static string GreetingsText
         {
-            base.OnCreate(savedInstanceState);
-
-			_textureView = new TextureView(this);
-			_textureView.SurfaceTextureListener = this;
-
-			SetContentView(_textureView);
-            //faart
-  
+            get;
+            set;
         }
 
-
-		public void OnSurfaceTextureAvailable(SurfaceTexture currentsurface, int w, int h)
-		{
-            //_camera.Open();
-
-			_textureView.LayoutParameters = new FrameLayout.LayoutParams(w, h);
-
-			//try
-			//{
-			//	_camera.SetPreviewTexture(surface);
-			//	_camera.StartPreview();
-
-			//}
-			//catch (Java.IO.IOException ex)
-			//{
-			//	System.Diagnostics.Debug.WriteLine(ex.Message);
-			//}
-
-			try
-			{
-				mImageReader = ImageReader.NewInstance(w, h, ImageFormatType.Jpeg, /*maxImages*/2);
-				//ImageReader.SetOnImageAvailableListener(mOnImageAvailableListener, mBackgroundHandler);
-
-				SurfaceTexture texture = currentsurface;
-				if (texture == null)
-				{
-					throw new IllegalStateException("texture is null");
-				}
-
-				// We configure the size of default buffer to be the size of camera preview we want.
-				texture.SetDefaultBufferSize(w, h);
-
-				// This is the output Surface we need to start preview.
-				Surface surface = new Surface(texture);
-
-				// We set up a CaptureRequest.Builder with the output Surface.
-				mPreviewRequestBuilder = _camera.CreateCaptureRequest(CameraTemplate.Preview);
-				mPreviewRequestBuilder.AddTarget(surface);
-
-				// Here, we create a CameraCaptureSession for camera preview.
-				var surfaces = new List<Surface>();
-				surfaces.Add(surface);
-				surfaces.Add(mImageReader.Surface);
-				_camera.CreateCaptureSession(surfaces, null, null);
-
-			}
-			catch (CameraAccessException e)
-			{
-				e.PrintStackTrace();
-			}
-		}
-
-        public void OnSurfaceTextureUpdated(Android.Graphics.SurfaceTexture surface)
+        public Tracker Create(Java.Lang.Object item)
         {
+            return new GraphicFaceTracker(mGraphicOverlay);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mPreview = FindViewById<CameraSourcePreview>(Resource.Id.preview);
+            mGraphicOverlay = FindViewById<GraphicOverlay>(Resource.Id.faceOverlay);
+            //greetingsText = FindViewById<TextView>(Resource.Id.greetingsTextView);
+
+
+            //if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Camera) == Permission.Granted)
+            //{
+                CreateCameraSource();
+                //LiveCamHelper.Init();
+                //LiveCamHelper.GreetingsCallback = (s) => { RunOnUiThread(() => GreetingsText = s); };
+                //await LiveCamHelper.RegisterFaces();
+            //}
+            //else { RequestCameraPermission(); }
+
+        }
+
+        private void CreateCameraSource()
+        {
+
+            var context = Application.Context;
+            FaceDetector detector = new FaceDetector.Builder(context)
+                    .SetClassificationType(ClassificationType.All)
+                    .Build();
+
+            detector.SetProcessor(
+                    new MultiProcessor.Builder(this)
+                            .Build());
+
+            if (!detector.IsOperational)
+            {
+                // Note: The first time that an app using face API is installed on a device, GMS will
+                // download a native library to the device in order to do detection.  Usually this
+                // completes before the app is run for the first time.  But if that download has not yet
+                // completed, then the above call will not detect any faces.
+                //
+                // isOperational() can be used to check if the required native library is currently
+                // available.  The detector will automatically become operational once the library
+                // download completes on device.
+                Log.Warn(TAG, "Face detector dependencies are not yet available.");
+            }
+
+            mCameraSource = new CameraSource.Builder(context, detector)
+                    .SetRequestedPreviewSize(640, 480)
+                    .SetFacing(CameraFacing.Back)
+                    .SetRequestedFps(30.0f)
+                    .Build();
 
 
         }
 
-		public void OnSurfaceTextureSizeChanged(Android.Graphics.SurfaceTexture surface, int w, int h)
-		{
+        class GraphicFaceTracker : Tracker
+        {
+            private GraphicOverlay mOverlay;
+            private FaceGraphic mFaceGraphic;
 
+            public GraphicFaceTracker(GraphicOverlay overlay)
+            {
+                mOverlay = overlay;
+                mFaceGraphic = new FaceGraphic(overlay);
+            }
 
-		}
+            public override void OnNewItem(int id, Java.Lang.Object item)
+            {
+                mFaceGraphic.SetId(id);
+            }
 
-		public bool OnSurfaceTextureDestroyed(Android.Graphics.SurfaceTexture surface)
-		{
-            _camera.Close();
-		
+            public override void OnUpdate(Detector.Detections detections, Java.Lang.Object item)
+            {
+                var face = item as Face;
+                mOverlay.Add(mFaceGraphic);
+                mFaceGraphic.UpdateFace(face);
+            }
 
-			return true;
-		}
+            public override void OnMissing(Detector.Detections detections)
+            {
+                mOverlay.Remove(mFaceGraphic);
+            }
 
+            public override void OnDone()
+            {
+                mOverlay.Remove(mFaceGraphic);
+            }
+        }
 
 
 
